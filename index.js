c097794189be96b29247da4897de4d05aedeca0c
thushar prasanth <thusharprasanth@gmail.com>
2023-09-24 16:58:34 +0530

    resolver function

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
 import { ApolloServer } from '@apollo/server';
 import { startStandaloneServer } from '@apollo/server/standalone';
+// import db
+import db from './_db.js'
 //importing typeDefs from schema.js
 import { typeDefs }  from './schema.js'
+
+const resolvers = {
+    Query : {  
+        games(){
+            return db.games
+        },
+        authors(){
+            return db.authors
+        },
+        reviews(){
+            return db.reviews
+        }
+    }
+}
 
 //server setup
 
 const server = new ApolloServer({
-    //typeDefs
-    typeDefs,
-    //Resolvers
+
+    typeDefs, //first argument
+    resolvers // second argument
+
 })
 
 const { url } = await startStandaloneServer(server, {
